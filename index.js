c516e5d08c202a9a9ceaa5a836191bb3268a4405
yusupha darboe <darboeyusuf22@gmail.com>
2026-05-30 15:22:52 +0300

    do it

--- index.js
+++ index.js
@@ -30,17 +30,13 @@ const app = express();
 app.use(express.json());
 app.use("/uploads", express.static("uploads/profile_images"));
 app.use("/uploads", express.static("uploads/patient_profile"));
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL
-];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
+app.use(cors({
+    origin: 'https://client-final-year.onrender.com',
+    // origin:'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-  })
-);
+}));
 await createUserTable();
 await createStaffTable();
 await createPatientsTable();
